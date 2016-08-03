c93806bbf2e7ded83483d30604f374109edff406
Karen Kochinyan <Karen Kochinyan>
2016-08-03 16:28:38 +0300

    Updated Circle class

--- Circle/src/main/java/Circle.java
+++ Circle/src/main/java/Circle.java
@@ -1,18 +1,27 @@
 import java.util.Objects;
+
 public class Circle {
+
     private final double radius;
+
     /**
      * @param radius - значение радиуса окружности
      */
     public Circle(double radius) {
+        //Радиус не может быть отрицательным, для этого надо выбрасывать исключение  IllegalArgumentException
+        if (radius < 0) {
+            throw new IllegalArgumentException("Radius cannot be negative.");
+        }
         this.radius = Objects.requireNonNull(radius);
     }
+
     /**
      * @return Площадь окружности
      */
     public double squareCircle() {
         return Math.PI * radius * radius;
     }
+
     /**
      * Метод проверяющий равны ли окружности по площадям
      *
@@ -23,10 +32,12 @@ public class Circle {
         Objects.requireNonNull(cir);
         return this.squareCircle() == cir.squareCircle();
     }
+
     @Override
     public String toString() {
         return "Circle{" +
                 "radius=" + radius +
                 '}';
     }
+
 }
